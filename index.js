ec7b3096c5d6f6c47b86b6160d46f37eb8d629b2
mandihamza <mandihamza@gmail.com>
2020-07-22 16:23:37 -0600

    Updates route config for resources

--- index.js
+++ index.js
@@ -27,6 +27,10 @@ server.use(bodyParser.urlencoded({ extended: true }));
 server.use(cookieParser())
 server.use(morgan())
 
+const corsOptions = {
+  origin: ['https://unsplash.com', 'https://unsplash.com/photos', 'https://source.unsplash.com/', 'http://localhost:3000']
+}
+
 // catch-all endpoint
 server.get('/', (req, res) => {
   res.status(200).json({ message: 'Welcome to the Dev Coach API' });
@@ -38,7 +42,11 @@ server.use('/profile', profileRouter);
 server.use('/payment', paymentRouter);
 server.use('/feedback', feedbackRouter);
 server.use('/video', videoRouter);
-server.use('/resources', resourcesRouter);
+// server.use('/resources', resourcesRouter);
+server.use('/resources', cors((corsOptions)), resourcesRouter, function (req, res, next) {
+  res.json({msg: 'This is CORS-enabled for a Single Route'})
+  next()
+})
 // server.use('/chat', chatRouter);
 // server.use('/editor', editorRouter);
 
